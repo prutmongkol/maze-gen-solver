ade198278e6c46d9e2f20023ec6107087d47d29e
prutmongkol <150164864+PrutMongkol@users.noreply.github.com>
2024-05-16 22:29:12 +0700

    add draw_move method in Cell class

--- src/cell.py
+++ src/cell.py
@@ -42,3 +42,18 @@ class Cell():
                 Point(x2, y2)
             )
             self._win.draw_line(line, "black")
+    
+    def draw_move(self, to_cell, undo=False) -> None:
+        start_point = Point(
+            (self._x1 + self._x2) // 2,
+            (self._y1 + self._y2) // 2
+        )
+        end_point = Point(
+            (to_cell._x1 + to_cell._x2) // 2,
+            (to_cell._y1 + to_cell._y2) // 2
+        )
+        line = Line(start_point, end_point)
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+        self._win.draw_line(line, fill_color)
--- src/main.py
+++ src/main.py
@@ -4,11 +4,33 @@ from cell import Cell
 
 def main():
     win = Window(800, 600)
-    # win.draw_line(Line(Point(0, 0), Point(250, 250)), "black")
-    cell = Cell(win)
-    cell.has_right_wall = False
-    cell.has_left_wall = False
-    cell.draw(50, 50, 100, 100)
+    
+    cell1 = Cell(win)
+    cell1.has_right_wall = False
+    cell1.has_left_wall = False
+    cell1.draw(50, 50, 100, 100)
+    
+    cell2 = Cell(win)
+    cell2.has_right_wall = False
+    cell2.has_left_wall = False
+    cell2.draw(100, 50, 150, 100)
+    
+    cell1.draw_move(cell2, True)
+    
+    cell3 = Cell(win)
+    cell3.has_left_wall = False
+    cell3.has_bottom_wall = False
+    cell3.draw(150, 50, 200, 100)
+    
+    cell2.draw_move(cell3)
+    
+    cell4 = Cell(win)
+    cell4.has_top_wall = False
+    cell4.has_left_wall = False
+    cell4.draw(150, 100, 200, 150)
+    
+    cell3.draw_move(cell4)
+    
     win.wait_for_close()
     
 
