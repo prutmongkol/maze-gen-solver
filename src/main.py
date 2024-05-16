45b373f61908139db5280d062987f17339a8e156
prutmongkol <150164864+PrutMongkol@users.noreply.github.com>
2024-05-16 21:42:53 +0700

    add Cell class

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
-from graphics import Window, Line, Point
+from graphics import Window
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    win.draw_line(Line(Point(0, 0), Point(250, 250)), "black")
+    # win.draw_line(Line(Point(0, 0), Point(250, 250)), "black")
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.has_left_wall = False
+    cell.draw(50, 50, 100, 100)
     win.wait_for_close()
     
 
